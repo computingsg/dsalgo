4d225b22a33f76c848cf9eabc49c4cb0c5dfbbb8
computingsg <92036307+computingsg@users.noreply.github.com>
2022-10-19 15:04:15 +0800

    Create carplate.py

---
+++ carplate.py
@@ -0,0 +1,3 @@
+# Create check digit validation problems (and solutions) 
+# from https://www.msn.com/en-sg/lifestyle/other/got-such-thing-in-singapore-ah-different-types-of-licence-plates/ar-AA11TfPk?ocid=entnewsntp
+
